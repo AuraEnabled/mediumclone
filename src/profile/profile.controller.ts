5fd1a2f4dab58a4b709c0600bc6664a8e8d2a84d
bell <boris.gribanov@gmail.com>
2024-01-14 01:53:42 +0200

    Lection 34; Implemented Follow Entity which represents following relation between two users

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { ProfileService } from '@app/profile/profile.service';
 import { User } from '@app/user/decorators/user.decorator';
 import { UserEntity } from '@app/user/user.entity';
 import { ProfileResponseInterface } from '@app/profile/types/profileResponse.interface';
 import { ProfileType } from '@app/profile/types/profile.type';
+import { UseGuards } from '@nestjs/common/decorators/core/use-guards.decorator';
+import { AuthGuard } from '@app/user/guards/auth.guard';
 
 @Controller('profiles')
 export class ProfileController {
@@ -15,6 +17,19 @@ export class ProfileController {
     @Param('username') username: string,
   ): Promise<ProfileResponseInterface> {
     const user: ProfileType = await this.profileService.getProfile(
+      currentUserId,
+      username,
+    );
+    return this.profileService.buildProfileResponse(user);
+  }
+
+  @Post(':username/follow')
+  @UseGuards(AuthGuard)
+  async followProfile(
+    @User('id') currentUserId: number,
+    @Param('username') username: string,
+  ): Promise<ProfileResponseInterface> {
+    const user: ProfileType = await this.profileService.followProfile(
       currentUserId,
       username,
     );
--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -6,12 +6,15 @@ import { ProfileResponseInterface } from '@app/profile/types/profileResponse.int
 import { ProfileType } from '@app/profile/types/profile.type';
 import { HttpStatus } from '@nestjs/common/enums/http-status.enum';
 import { HttpException } from '@nestjs/common/exceptions/http.exception';
+import { FollowEntity } from '@app/profile/follow.entity';
 
 @Injectable()
 export class ProfileService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
   ) {}
 
   async getProfile(currentUserId: number, username: string): Promise<ProfileType> {
@@ -24,6 +27,39 @@ export class ProfileService {
     }
 
     return { ...requestedUser, following: false } as ProfileType;
+  }
+
+  async followProfile(
+    currentUserId: number,
+    username: string,
+  ): Promise<ProfileType> {
+    const requestedUser = await this.userRepository.findOne({
+      where: { username: username },
+    });
+
+    if (!requestedUser) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === requestedUser.id) {
+      throw new HttpException(
+        "Follower and following one can't be the same",
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: { followerId: currentUserId, followingId: requestedUser.id },
+    });
+
+    if (!follow) {
+      const followToCreate = new FollowEntity();
+      followToCreate.followerId = currentUserId;
+      followToCreate.followingId = requestedUser.id;
+      this.followRepository.save(followToCreate);
+    }
+
+    return { ...requestedUser, following: true };
   }
 
   buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
